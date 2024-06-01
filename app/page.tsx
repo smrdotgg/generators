ec3cac09ef1dcaccd387792bcce326c4e579ed20
Semere Tereffe <se.semere.tereffe@gmail.com>
2024-06-01 16:12:30 +0300

    Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { generatorComponent } from "./generator";
 
+export const dynamic = 'force-dynamic'
+
 export default generatorComponent(async function* () {
   yield <div> Fetching tweets ...</div>
   await new Promise((resolve) => setTimeout(resolve, 3000));
